5b5393d5dbed95fd655da1d11156c07770274caf
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-04-23 18:57:04 +0300

    created new contract, to control ownerFee and transfer commision to owner

---
+++ contracts/OwnerAccess.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract OwnerAccess {
+	using Address for address payable;
+
+	address public owner;
+  	uint256 public ownerFee;
+
+	constructor(uint256 _ownerFee) {
+		owner = msg.sender;
+		setOwnerFee(_ownerFee);
+	}
+
+	function setOwnerFee(uint256 _newOwnerFee) onlyOwner public {
+		ownerFee = _newOwnerFee;
+	}
+
+	function collectFees() onlyOwner external {
+		payable(owner).sendValue(address(this).balance);
+	}
+
+	modifier onlyOwner {
+		require(msg.sender == owner, "You are not an owner");
+		_;
+	}
+}
